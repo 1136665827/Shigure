36f55441985d6d1bade32fbea66aabbff3aeea0c
waynebian01 <bian.wayne@gmail.com>
2026-08-23 13:57:58 +0800

    添加愤怒之锤的法术ID

--- Fuyutsui/class/Paladin.lua
+++ Fuyutsui/class/Paladin.lua
@@ -352,5 +352,8 @@ Fuyutsui.spellsList = {
     [6940]    = { index = 27, name = "牺牲祝福" },
     [391054]  = { index = 28, name = "代祷" },
     [633]     = { index = 29, name = "圣疗术" },
-    [35395]   = { index = 30, name = "十字军打击" }
+    [35395]   = { index = 30, name = "十字军打击" },
+    [1241288] = { index = 31, name = "愤怒之锤" },
+    [1277026] = { index = 32, name = "愤怒之锤" },
+    [1279408] = { index = 33, name = "愤怒之锤" },
 }
